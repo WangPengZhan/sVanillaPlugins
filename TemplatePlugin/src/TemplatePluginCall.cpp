3e6d12b69bf4d1679c2054f4b50dd6ca1fe834c3
iveswang <pengzhan.wang@zd-automotive.cn>
2024-04-08 14:10:23 +0800

    fix plugin bugs

--- TemplatePlugin/src/TemplatePluginCall.cpp
+++ TemplatePlugin/src/TemplatePluginCall.cpp
@@ -4,17 +4,36 @@
 
 TemplatePlugin *pPlugin = new TemplatePlugin;
 
-Plugin::IPlugin *plugin()
+PIPlugin plugin()
 {
-    return pPlugin;
+    return reinterpret_cast<PIPlugin>(pPlugin);
 }
 
 const char *pluginName()
 {
+    if (!pPlugin)
+    {
+        return "";
+    }
+
     return pPlugin->pluginName().c_str();
 }
 
 const char *pluginVersion()
 {
+    if (!pPlugin)
+    {
+        return "";
+    }
+
     return pPlugin->pluginVersion().c_str();
+}
+
+void  pluginDeinit()
+{    
+    if (pPlugin)
+    {
+        delete pPlugin;
+        pPlugin = nullptr;
+    }
 }
